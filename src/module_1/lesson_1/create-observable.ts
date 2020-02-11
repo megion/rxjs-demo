d2dbd9e3e1ba0f363f1840167768a17a0f8584b2
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2020-02-11 09:38:00 +0300

    createObservable demo

--- src/module_1/lesson_1/create-observable.ts
+++ src/module_1/lesson_1/create-observable.ts
@@ -1,4 +1,4 @@
-import { Observable, Observer } from "rxjs"
+import { Observable, Observer, of, throwError } from "rxjs"
 //import { switchMap } from "rxjs/operators"
 
 export function createObservable(): void {
@@ -7,11 +7,17 @@ export function createObservable(): void {
    */
   const hello = Observable.create(function(observer: Observer<string>) {
     observer.next("Hello")
+    //throw Error("my error")
     observer.next("World")
     //observer.complete()
     setTimeout(() => {
-      observer.next("bla bla")
-      observer.complete()
+      try {
+        throw Error("my error")
+        observer.next("bla bla")
+        observer.complete()
+      } catch (err) {
+        observer.error(err)
+      }
     }, 1000)
   })
 
@@ -28,4 +34,37 @@ export function createObservable(): void {
     },
   })
   console.log("just after subscribe")
+
+  // create Observable from object.
+  // like Promise.resolve
+  const observableOf = of(4)
+  observableOf.subscribe({
+    next(x: number) {
+      console.log("observableOf: next value: " + x)
+    },
+    error(err) {
+      console.error("observableOf: error: " + err)
+    },
+    complete() {
+      console.log("observableOf: complete")
+    },
+  })
+  observableOf.subscribe((x: number) => {
+    console.log("observableOf2: next value: " + x)
+  })
+  console.log("after subscribe observableOf")
+
+  // demo throwError
+  const myError = throwError("My error")
+  myError.subscribe({
+    next(x: number) {
+      console.log("myError: next value: " + x)
+    },
+    error(err) {
+      console.error("myError: error: " + err)
+    },
+    complete() {
+      console.log("myError: complete")
+    },
+  })
 }
